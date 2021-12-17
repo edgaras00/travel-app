033077c0f5cef77ab602762361b62d47b012160c
Edgar <edgaras993@gmail.com>
2021-12-17 14:48:40 -0500

    added error handling middleware

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const userRouter = require("./api/routes/userRouter");
 const bookingRouter = require("./api/routes/bookingRouter");
 const reviewRouter = require("./api/routes/reviewRouter");
 const tourRouter = require("./api/routes/tourRouter");
+const AppError = require("./api/utils/appError");
+const errorHandler = require("./api/controllers/errorController");
 
 const app = express();
 
@@ -18,5 +20,11 @@ app.use("/api/users", userRouter);
 app.use("/api/reviews", reviewRouter);
 app.use("/api/tours", tourRouter);
 app.use("/api/bookings", bookingRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Route ${req.originalUrl} is not found`, 404));
+});
+
+app.use(errorHandler);
 
 module.exports = app;
