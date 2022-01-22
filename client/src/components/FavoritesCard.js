56d15e30109968c33bd0403e1f9724450b0cabae
Edgar <edgaras993@gmail.com>
2022-01-21 23:17:42 -0500

    Added mouseover/out events to modifyclass for styling

--- client/src/components/FavoritesCard.js
+++ client/src/components/FavoritesCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/favoriteCard.css";
 
@@ -11,14 +11,28 @@ const FavoritesCard = ({
   duration,
   path,
 }) => {
+  const [hoverClass, setHoverClass] = useState(null);
+
+  const handleMouseOver = () => {
+    setHoverClass("hovered");
+  };
+
+  const handleMouseOut = () => {
+    setHoverClass(null);
+  };
+
   return (
-    <div className="favorites-card">
+    <div
+      className={"favorites-card"}
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
+    >
       <Link to={path}>
         <div className="favorite-card-content">
           <div className="favorite-image-wrapper">
             <img src={image} alt="Traveler favorite destination" />
           </div>
-          <div className="favorites-text-container">
+          <div className={`favorites-text-container ${hoverClass}`}>
             <div className="favorites-title">
               <div className="title-top">{city}</div>
               <div className="title-country">{country}</div>
