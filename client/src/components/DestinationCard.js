7a0b8468025a801147f767d7a8d3ec78a2b4b9e4
Edgar <edgaras993@gmail.com>
2022-01-21 23:41:49 -0500

    added hover events on destination cards

--- client/src/components/DestinationCard.js
+++ client/src/components/DestinationCard.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/destinationCard.css";
 
 const DestinationCard = ({ image, name, size }) => {
-  const style = {
-    backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.5) ), url(${image})`,
+  const [cardStyle, setCardStyle] = useState(
+    `linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.5) ), url(${image})`
+  );
+
+  const handleMouseOver = () => {
+    setCardStyle(
+      `linear-gradient( rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.3) ), url(${image})`
+    );
   };
 
+  const handleMouseOut = () => {
+    setCardStyle(
+      `linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.5) ), url(${image})`
+    );
+  };
+
+  // const style = {
+  //   // backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.5) ), url(${image})`,
+  //   backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.5) ), url(${image})`,
+  // };
+
   return (
-    <div className={`destination-card ${size}`} style={style}>
+    <div
+      className={`destination-card ${size}`}
+      style={{ backgroundImage: cardStyle }}
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
+    >
       <div className="destination-name">{name}</div>
     </div>
   );
